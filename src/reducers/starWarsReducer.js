b8673454ee23cfb419696dc211fc58cd38a774fb
NightlyD3V <TylerSpaulding95@gmail.com>
2019-04-10 20:37:48 +0100

    Cannot read property of map error working to solve problem

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,30 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING_DATA, SUCCESS, FAILURE } /* we need our action types here*/ from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
   // Array characters, Boolean fetching, null error.
+  error: '',
+  isFetching: false
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING_DATA: 
+      return {
+        ...state,
+        isFetching: true
+      }
+    case SUCCESS:
+      return {
+        ...state,
+        payload: action.payload 
+      }
+    case FAILURE: 
+      return {
+        ...state,
+        error: 'YOU FAILED!'
+      }
     default:
       return state;
   }
